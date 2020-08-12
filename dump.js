218d788393f25bd0fb3fe98626e239fac05737d4
wesley <350030173@qq.com>
2020-08-12 12:21:54 +0800

    Update dump.js

--- dump.js
+++ dump.js
@@ -1,3 +1,40 @@
+//
+//获取解密后的global-metadata.dat
+//
+//用法：
+//frida -U -l global-metadata_dump.js packagename
+//
+//导出的文件在/data/data/yourPackageName/global-metadata.dat
+//
+//get_self_process_name()获取当前运行进程包名
+//参考：https://github.com/lasting-yang/frida_dump/blob/master/dump_dex_class.js
+//
+//
+function get_self_process_name() {
+    var openPtr = Module.getExportByName('libc.so', 'open');
+    var open = new NativeFunction(openPtr, 'int', ['pointer', 'int']);
+
+    var readPtr = Module.getExportByName("libc.so", "read");
+    var read = new NativeFunction(readPtr, "int", ["int", "pointer", "int"]);
+
+    var closePtr = Module.getExportByName('libc.so', 'close');
+    var close = new NativeFunction(closePtr, 'int', ['int']);
+
+    var path = Memory.allocUtf8String("/proc/self/cmdline");
+    var fd = open(path, 0);
+    if (fd != -1) {
+        var buffer = Memory.alloc(0x1000);
+
+        var result = read(fd, buffer, 0x1000);
+        close(fd);
+        result = ptr(buffer).readCString();
+        return result;
+    }
+
+    return "-1";
+}
+
+
 function frida_Memory(pattern)
 {
 	Java.perform(function ()
@@ -20,7 +57,7 @@ function frida_Memory(pattern)
 						header: true,
 						ansi: true
 					}));
-					
+					//0x108，0x10C如果不行，换0x100，0x104
 					var DefinitionsOffset = parseInt(address, 16) + 0x108;
 					var DefinitionsOffset_size = Memory.readInt(ptr(DefinitionsOffset));
 					
@@ -29,8 +66,8 @@ function frida_Memory(pattern)
 					
 					//根据两个偏移得出global-metadata大小
 					var global_metadata_size = DefinitionsOffset_size + DefinitionsCount_size
-					console.log("global-metadata size：",global_metadata_size);
-					var file = new File("/data/data/你的包名/global-metadata.dat", "wb");
+					console.log("大小：",global_metadata_size);
+					var file = new File("/data/data/" + get_self_process_name() + "/global-metadata.dat", "wb");
 					file.write(Memory.readByteArray(address, global_metadata_size));
 					file.flush();
 					file.close();
